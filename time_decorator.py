314e59d9dcfb5b8c818728f0053fbf085912a0d7
Savchenko-Ilia <savchenko.ilia@gmail.com>
2014-11-12 18:05:16 +0200

    time decorator on Python
    this is test file for using github

---
+++ time_decorator.py
@@ -0,0 +1,17 @@
+﻿import time
+def timer(f):
+    def tmp(*args, **kwargs):
+        t = time.time()
+        res = f(*args, **kwargs)
+        print "Время выполнения функции: %f" % (time.time()-t)
+        return res
+
+    return tmp
+
+@timer
+def func(x, y):
+    return x + y
+
+@timer
+def func1(x, y):
+    return x / y
